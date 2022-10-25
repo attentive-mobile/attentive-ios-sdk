8c3c6d472b111a17f1cce502b0b63694f848429b
Olivia <oliviakim321@gmail.com>
2022-10-25 11:22:20 -0400

    transparent webview

--- Creative/ATTNSDK.m
+++ Creative/ATTNSDK.m
@@ -62,6 +62,10 @@
     
     _webView = [[WKWebView alloc] initWithFrame:theView.frame configuration:wkWebViewConfiguration];
     _webView.navigationDelegate = self;
+
+    _webView.opaque = NO;
+    _webView.backgroundColor = [UIColor clearColor];
+
     [_webView loadRequest:request ];
     
     if ([_mode isEqual:@"debug"]) {
@@ -80,8 +84,8 @@
     "return iframe;";
 
     [webView callAsyncJavaScript:asyncJs arguments:nil inFrame:nil inContentWorld:WKContentWorld.defaultClientWorld completionHandler:^(NSString *id, NSError *error) {
-        if ([id isEqual:@"attentive_creative"] && ![_mode isEqual:@"debug"]) {
-            [_parentView addSubview:webView];
+        if ([id isEqual:@"attentive_creative"] && ![self->_mode isEqual:@"debug"]) {
+            [self->_parentView addSubview:webView];
         }
     }];
 }
