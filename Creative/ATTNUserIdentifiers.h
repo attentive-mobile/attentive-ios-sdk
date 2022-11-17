9925850aa21f113233e28d72b80bdfb85911608f
Olivia <oliviakim321@gmail.com>
2022-11-17 12:49:01 -0500

    docs

--- Creative/ATTNUserIdentifiers.h
+++ Creative/ATTNUserIdentifiers.h
@@ -11,11 +11,22 @@
 
 - (nonnull id)initWithUserIdentifiers:(nonnull NSDictionary *) userIdentifiers;
 
+// Your unique identifier for the user - this should be consistent across the user's lifetime, for example a database id
 @property (readonly, nonnull) NSString * clientUserId;
+
+// The user's email
 @property (readonly, nullable) NSString * email;
+
+// The user's phone number in E.164 format
 @property (readonly, nullable) NSString * phone;
+
+// The user's Klaviyo ID
 @property (readonly, nullable) NSString * klaviyoId;
+
+// The user's Shopify Customer ID
 @property (readonly, nullable) NSString * shopifyId;
+
+// Key-value pairs of custom identifier names and values to associate with this user
 @property (readonly, nullable) NSDictionary<NSString *, NSString *> * customIdentifiers;
 
 @end
