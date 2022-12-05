d9fef8d8c935f138f425a42bb84c90a2bc70e1cd
Wyatt Davis <wdavis@attentivemobile.com>
2022-12-05 09:46:35 -0800

    Fixes ios13

--- Sources/ATTNSDK.m
+++ Sources/ATTNSDK.m
@@ -50,6 +50,12 @@
 - (void)trigger:(UIView *)theView {
     _parentView = theView;
     NSLog(@"Called showWebView in creativeSDK with domain: %@", _domain);
+    if (@available(iOS 14, *)) {
+        NSLog(@"The iOS version is new enough, continuing to show the Attentive creative.");
+    } else {
+        NSLog(@"Not showing the Attentive creative because the iOS version is too old.");
+        return;
+    }
     NSString* creativePageUrl = [self buildCompanyCreativeUrl];
 
     NSLog(@"Requesting creative page url: %@", creativePageUrl);
