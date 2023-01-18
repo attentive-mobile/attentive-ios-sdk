65a2177758f8aab07fd3ab4e135ab94747702e39
Wyatt Davis <wdavis@attentivemobile.com>
2023-01-18 12:06:06 -0800

    wip

---
+++ Sources/ATTNCustomEvent.h
@@ -0,0 +1,16 @@
+//
+//  ATTNCustomEvent.h
+//  attentive-ios-sdk
+//
+//  Created by Wyatt Davis on 1/17/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ATTNCustomEvent : NSObject<ATTEvent>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/ATTNCustomEvent.m
@@ -0,0 +1,12 @@
+//
+//  ATTNCustomEvent.m
+//  attentive-ios-sdk
+//
+//  Created by Wyatt Davis on 1/17/23.
+//
+
+#import "ATTNCustomEvent.h"
+
+@implementation ATTNCustomEvent
+
+@end
