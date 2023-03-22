fad545b1219fc592f05adb113cff4b2d4240e7a7
Wyatt Davis <wdavis@attentivemobile.com>
2023-03-22 14:48:16 -0700

    Adds working CustomEvent

--- Sources/ATTNCustomEvent.m
+++ Sources/ATTNCustomEvent.m
@@ -2,11 +2,20 @@
 //  ATTNCustomEvent.m
 //  attentive-ios-sdk
 //
-//  Created by Wyatt Davis on 1/17/23.
+//  Created by Wyatt Davis on 3/20/23.
 //
 
 #import "ATTNCustomEvent.h"
 
 @implementation ATTNCustomEvent
+
+- (instancetype)initWithType:(NSString*)type properties:(NSDictionary<NSString*, NSString*>*)properties {
+    if (self = [super init]) {
+        _type = type;
+        _properties = [[NSDictionary alloc] initWithDictionary:properties];
+    }
+    
+    return self;
+}
 
 @end
