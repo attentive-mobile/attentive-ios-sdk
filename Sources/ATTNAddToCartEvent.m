35265379dad6a966412df6d1c811287524ae8e35
Olivia <oliviakim321@gmail.com>
2023-03-28 11:09:34 -0400

    Revert "lint"
    This reverts commit a115827b4a916c50f33e9c8b4d717a7a4b78c477.

--- Sources/ATTNAddToCartEvent.m
+++ Sources/ATTNAddToCartEvent.m
@@ -5,19 +5,19 @@
 //  Created by Wyatt Davis on 1/17/23.
 //
 
-#import "ATTNAddToCartEvent.h"
 #import "ATTNEvent.h"
+#import "ATTNAddToCartEvent.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @implementation ATTNAddToCartEvent
 
 - (instancetype)initWithItems:(NSArray<ATTNItem *> *)items {
-  if (self = [super init]) {
-    self->_items = items;
-  }
-
-  return self;
+    if (self = [super init]) {
+        self->_items = items;
+    }
+    
+    return self;
 }
 
 @end
--- Sources/ATTNProductViewEvent.m
+++ Sources/ATTNProductViewEvent.m
@@ -5,19 +5,19 @@
 //  Created by Wyatt Davis on 1/17/23.
 //
 
-#import "ATTNProductViewEvent.h"
 #import "ATTNEvent.h"
+#import "ATTNProductViewEvent.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @implementation ATTNProductViewEvent
 
 - (instancetype)initWithItems:(NSArray<ATTNItem *> *)items {
-  if (self = [super init]) {
-    self->_items = items;
-  }
-
-  return self;
+    if (self = [super init]) {
+        self->_items = items;
+    }
+    
+    return self;
 }
 
 @end
