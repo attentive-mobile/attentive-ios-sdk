52a682e4e7cc8b6b2713efee7a010c53213de53c
Olivia <oliviakim321@gmail.com>
2023-03-28 12:40:36 -0400

    lint script

---
+++ lint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+HOST=${1:-local}
+
+
+FILES_TO_LINT=$(find . \( \
+  -path "./Vendor/*" -prune -o \
+  -path "./*/Pods/*" -prune -o \
+  -path "./DerivedData/*" -prune -o \
+  -path "./build/*" -prune -o \
+  -path "./*/DerivedData/*" -prune -o \
+  -path "./*/build/*" -prune -o \
+  -path "./attentive-ios-sdk.xc*" -prune \
+  \) \
+  -o -name "*.h" -o -name "*.m" -print)
+
+
+case "$HOST" in
+  local)
+    clang-format-11 -i --assume-filename=Objective-C $FILES_TO_LINT
+    ;;
+
+  ci)
+    clang-format --dry-run --Werror --assume-filename=Objective-C
+    ;;
+  *)
+    echo "invalid host name"
+    ;;
+esac
