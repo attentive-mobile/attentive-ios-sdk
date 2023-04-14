116b50eb174e6d3b1c12783514b56b84668b73f2
Wyatt Davis <wdavis@attentivemobile.com>
2023-04-14 12:14:23 -0700

    lint

--- Tests/ATTNCustomEventTest.m
+++ Tests/ATTNCustomEventTest.m
@@ -15,22 +15,22 @@
 @implementation ATTNCustomEventTest
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+  // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
 - (void)testConstructor {
-  XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:nil properties:@{@"k": @"v"}]);
-  XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:@"" properties:@{@"k": @"v"}]);
-  XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:@"]" properties:@{@"k": @"v"}]);
+  XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:nil properties:@{@"k" : @"v"}]);
+  XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:@"" properties:@{@"k" : @"v"}]);
+  XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:@"]" properties:@{@"k" : @"v"}]);
   XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:@"good" properties:nil]);
-  XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:@"good" properties:@{@"k]": @"v"}]);
-  
+  XCTAssertThrows([[ATTNCustomEvent alloc] initWithType:@"good" properties:@{@"k]" : @"v"}]);
+
   XCTAssertNoThrow([[ATTNCustomEvent alloc] initWithType:@"good" properties:@{}]);
-  XCTAssertNoThrow([[ATTNCustomEvent alloc] initWithType:@"good" properties:@{@"k": @"v"}]);
+  XCTAssertNoThrow([[ATTNCustomEvent alloc] initWithType:@"good" properties:@{@"k" : @"v"}]);
 }
 
 @end
