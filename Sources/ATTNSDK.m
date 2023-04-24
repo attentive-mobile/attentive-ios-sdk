ccbfbe97e94a9149c7310bdaadf0dbfb2722f3a1
Olivia <oliviakim321@gmail.com>
2023-04-24 17:01:03 -0400

    rename

--- Sources/ATTNSDK.m
+++ Sources/ATTNSDK.m
@@ -13,15 +13,15 @@
 #import "Internal/ATTNInfoEvent.h"
 
 
-// Status passed to ATTNTriggerCompletionHandler when the creative is opened sucessfully
+// Status passed to ATTNCreativeTriggerCompletionHandler when the creative is opened sucessfully
 NSString *const CREATIVE_TRIGGER_STATUS_OPENED = @"CREATIVE_TRIGGER_STATUS_OPENED";
-// Status passed to ATTNTriggerCompletionHandler when the creative is closed sucessfully
+// Status passed to ATTNCreativeTriggerCompletionHandler when the creative is closed sucessfully
 NSString *const CREATIVE_TRIGGER_STATUS_CLOSED = @"CREATIVE_TRIGGER_STATUS_CLOSED";
-// Status passed to the ATTNTriggerCompletionHandler when the Creative has been triggered but it is not
+// Status passed to the ATTNCreativeTriggerCompletionHandler when the Creative has been triggered but it is not
 // opened successfully. This can happen if there is no available mobile app creative, if the creative
 // is fatigued, if the creative call has been timed out, or if an unknown exception occurs.
 NSString *const CREATIVE_TRIGGER_STATUS_NOT_OPENED = @"CREATIVE_TRIGGER_STATUS_NOT_OPENED";
-// Status passed to the ATTNTriggerCompletionHandler when the Creative is not closed due to an unknown
+// Status passed to the ATTNCreativeTriggerCompletionHandler when the Creative is not closed due to an unknown
 // exception
 NSString *const CREATIVE_TRIGGER_STATUS_NOT_CLOSED = @"CREATIVE_TRIGGER_STATUS_NOT_CLOSED";
 
