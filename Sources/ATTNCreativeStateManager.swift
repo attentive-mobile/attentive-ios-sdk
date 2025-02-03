d7effaaf67a46af9d4e4d98bf80fe61b8b235bc1
Adela Gao <adelag@attentivemobile.com>
2025-02-03 10:48:01 -0800

    Refactored web view and creative showing flags to improve thread synchronization and prevent race conditions

---
+++ Sources/ATTNCreativeStateManager.swift
@@ -0,0 +1,34 @@
+//
+//  ATTNCreativeStateManager.swift
+//  attentive-ios-sdk-framework
+//
+//  Created by Adela Gao on 2/18/25.
+//
+
+import Foundation
+
+/// A thread-safe manager for creative state transitions. it uses a concurrent dispatch queue with barrier writes to ensure thread safety
+/// Note: This will be refactored to an actor in a future swift concurrency migration
+///
+enum CreativeState {
+  case closed
+  case launching
+  case open
+}
+
+final class ATTNCreativeStateManager {
+
+  static let shared = ATTNCreativeStateManager()
+  private var state: CreativeState = .closed
+  private let queue = DispatchQueue(label: "com.attentive.creativeStateQueue", attributes: .concurrent)
+
+  func getState() -> CreativeState {
+    return queue.sync { state }
+  }
+
+  func updateState(_ newState: CreativeState) {
+    queue.async(flags: .barrier) {
+      self.state = newState
+    }
+  }
+}
---
+++ Tests/TestCases/ATTNWebViewHandlerTests.swift
@@ -0,0 +1,212 @@
+//
+//  ATTNWebViewHandlerTests.swift
+//  attentive-ios-sdk Tests
+//
+//  Created by Adela Gao on 1/28/25.
+//
+
+// MARK: Integration tests
+
+
+import Foundation
+import UIKit
+import WebKit
+import XCTest
+
+@testable import ATTNSDKFramework
+
+// MARK: Integration tests
+
+final class ATTNWebViewHandlerIntegrationTests: XCTestCase {
+
+  var mockWebViewProvider: MockWebViewProvider!
+  var mockUrlProvider: MockCreativeUrlProvider!
+  var handler: ATTNWebViewHandler!
+
+  override func setUp() {
+    super.setUp()
+    mockWebViewProvider = MockWebViewProvider()
+    mockUrlProvider = MockCreativeUrlProvider()
+    handler = ATTNWebViewHandler(
+      webViewProvider: mockWebViewProvider,
+      creativeUrlBuilder: mockUrlProvider,
+      stateManager: ATTNCreativeStateManager()
+    )
+  }
+
+  override func tearDown() {
+    mockWebViewProvider = nil
+    mockUrlProvider = nil
+    handler = nil
+    super.tearDown()
+  }
+
+  func testLaunchCreative_ShouldAddWebView() {
+    let parentView = UIView()
+
+    let expectation = self.expectation(description: "WebView should be added")
+    mockWebViewProvider.webViewSetupExpectation = expectation
+
+    handler.launchCreative(parentView: parentView, creativeId: "testCreative")
+
+    waitForExpectations(timeout: 5.0) { error in
+      XCTAssertNil(error, "WebView was not added in time")
+    }
+
+    XCTAssertNotNil(mockWebViewProvider.webView, "WebView should be initialized")
+    XCTAssertEqual(mockWebViewProvider.parentView, parentView, "Parent view should be set correctly")
+  }
+
+  func testLaunchCreative_ShouldPreventRaceConditions() {
+    let parentView = UIView()
+    let expectation = self.expectation(description: "WebView should be set")
+
+    expectation.expectedFulfillmentCount = 1 // Only one WebView should be created
+
+    mockWebViewProvider.webViewSetupExpectation = expectation
+
+    DispatchQueue.global().async {
+      print("Dispatch 1 - Launching creative 1")
+      self.handler.launchCreative(parentView: parentView, creativeId: "raceCondition1")
+    }
+
+    DispatchQueue.global().async {
+      print("Dispatch 2 - Launching creative 2")
+      self.handler.launchCreative(parentView: parentView, creativeId: "raceCondition2")
+    }
+
+    waitForExpectations(timeout: 5.0) { error in
+      if let error = error {
+        print("Expectation not fulfilled: \(error.localizedDescription)")
+      }
+      XCTAssertNil(error, "Race condition was not handled correctly")
+    }
+
+    print("WebView creation count: \(mockWebViewProvider.webViewCreationCount)")
+    XCTAssertEqual(mockWebViewProvider.webViewCreationCount, 1, "WebView should be created only once")
+  }
+
+  func testCloseCreative_ShouldRemoveWebView() {
+    let parentView = UIView()
+
+    handler.launchCreative(parentView: parentView, creativeId: "testCreative")
+
+    let expectation = self.expectation(description: "WebView should be removed")
+    mockWebViewProvider.webViewRemovalExpectation = expectation
+
+    handler.closeCreative()
+
+    waitForExpectations(timeout: 5.0) { error in
+      XCTAssertNil(error, "WebView was not removed in time")
+    }
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {  // Wait to check for unexpected re-creation
+      XCTAssertNil(self.mockWebViewProvider?.webView, "WebView should be nil after closing creative")
+    }
+  }
+
+  func testLaunchCreative_ShouldLoadCorrectURL() {
+    let parentView = UIView()
+    let creativeId = "testCreative"
+    let expectedURL = "https://mockurl.com/creative?id=\(creativeId)"
+
+    handler = TestWebViewHandler(
+      webViewProvider: mockWebViewProvider,
+      creativeUrlBuilder: MockCreativeUrlProvider(),
+      stateManager: ATTNCreativeStateManager.shared
+    )
+    mockWebViewProvider.webViewSetupExpectation = expectation(description: "WebView should be set up and load the URL")
+
+    handler.launchCreative(parentView: parentView, creativeId: creativeId)
+
+    waitForExpectations(timeout: 5.0) { error in
+      XCTAssertNil(error, "WebView did not set up in time")
+      let actualURL = (self.mockWebViewProvider.webView as? MockWKWebView)?.loadedURL
+      XCTAssertEqual(actualURL, expectedURL, "WebView should load the correct URL")
+    }
+  }
+}
+
+// MARK: Mocks
+
+// A custom WKWebView subclass that records the URL when load(_:) is called.
+class MockWKWebView: WKWebView {
+  var loadedURL: String?
+
+  override func load(_ request: URLRequest) -> WKNavigation? {
+    loadedURL = request.url?.absoluteString
+    return nil
+  }
+}
+
+class MockWebViewProvider: NSObject, ATTNWebViewProviding {
+  var parentView: UIView?
+  var skipFatigueOnCreative: Bool = false
+  var triggerHandler: ATTNCreativeTriggerCompletionHandler?
+
+  private(set) var getDomainCallCount = 0
+  private(set) var getModeCallCount = 0
+  private(set) var getUserIdentityCallCount = 0
+
+  var mockDomain: String = "mock.domain.com"
+  var mockMode: ATTNSDKMode = .debug
+  var mockUserIdentity: ATTNUserIdentity = .init()
+
+  // XCTestExpectations for async behavior
+  var webViewSetupExpectation: XCTestExpectation?
+  var webViewRemovalExpectation: XCTestExpectation?
+
+  private var _webView: WKWebView?
+  var webViewCreationCount = 0
+  var loadedURL: String?
+
+  // Observers to trigger expectations
+  var webView: WKWebView? {
+    didSet {
+      if let webView = webView {
+        webViewCreationCount += 1
+        // If it's a MockWKWebView, capture its loaded URL.
+        if let mockWebView = webView as? MockWKWebView {
+          self.loadedURL = mockWebView.loadedURL
+        }
+        DispatchQueue.main.async {
+          self.webViewSetupExpectation?.fulfill()
+        }
+      } else {
+        DispatchQueue.main.async {
+          self.webViewRemovalExpectation?.fulfill()
+        }
+      }
+    }
+  }
+
+  func getDomain() -> String {
+    getDomainCallCount += 1
+    return mockDomain
+  }
+
+  func getMode() -> ATTNSDKMode {
+    getModeCallCount += 1
+    return mockMode
+  }
+
+  func getUserIdentity() -> ATTNUserIdentity {
+    getUserIdentityCallCount += 1
+    return mockUserIdentity
+  }
+}
+
+class MockCreativeUrlProvider: ATTNCreativeUrlProviding {
+  var mockUrl: String = "https://mockurl.com/creative?id=default"
+
+  func buildCompanyCreativeUrl(configuration: ATTNCreativeUrlConfig) -> String {
+    return "https://mockurl.com/creative?id=\(configuration.creativeId ?? "default")"
+  }
+}
+
+class TestWebViewHandler: ATTNWebViewHandler {
+  override func makeWebView() -> WKWebView {
+    return MockWKWebView(frame: .zero, configuration: WKWebViewConfiguration())
+  }
+}
+
