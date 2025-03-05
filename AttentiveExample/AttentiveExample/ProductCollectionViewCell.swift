d0df140a84fbd16f459653a3cbc8cea409f9f00e
Adela Gao <adelag@attentivemobile.com>
2025-03-05 09:54:21 -0800

    added product cells and basic placeholder info for product names

---
+++ AttentiveExample/AttentiveExample/ProductCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProductCollectionViewCell.swift
+//  AttentiveExample
+//
+//  Created by Adela Gao on 3/5/25.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+}
