b9e43f690deb62a8af6c968e1e01229bf2b76b6f
Adela Gao <adelag@attentivemobile.com>
2025-03-05 16:34:27 -0800

    refactored to using mvvm going forward

--- AttentiveExample/AttentiveExample/CartViewController.swift
+++ AttentiveExample/AttentiveExample/CartViewController.swift
@@ -9,28 +9,40 @@ import UIKit
 import ATTNSDKFramework
 
 class CartViewController: UIViewController {
-  
+
   private let tableView = UITableView()
-  var cartProducts: [ATTNItem] = []
-  
+  private let viewModel: ProductListViewModel
+
+  init(viewModel: ProductListViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: - Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     title = "My Cart"
     view.backgroundColor = .white
-    
+
     setupTableView()
     setupCheckoutButton()
+
   }
-  
+
   private func setupTableView() {
     tableView.translatesAutoresizingMaskIntoConstraints = false
     tableView.delegate = self
     tableView.dataSource = self
     tableView.register(CartTableViewCell.self, forCellReuseIdentifier: "CartCell")
-    
+
     view.addSubview(tableView)
-    
+
     NSLayoutConstraint.activate([
       tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
       tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -38,7 +50,7 @@ class CartViewController: UIViewController {
       tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -60) // Leaves space for checkout button
     ])
   }
-  
+
   private func setupCheckoutButton() {
     let checkoutButton = UIButton(type: .system)
     checkoutButton.setTitle("Check Out", for: .normal)
@@ -47,10 +59,10 @@ class CartViewController: UIViewController {
     checkoutButton.setTitleColor(.white, for: .normal)
     checkoutButton.layer.cornerRadius = 10
     checkoutButton.addTarget(self, action: #selector(checkoutTapped), for: .touchUpInside)
-    
+
     checkoutButton.translatesAutoresizingMaskIntoConstraints = false
     view.addSubview(checkoutButton)
-    
+
     NSLayoutConstraint.activate([
       checkoutButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
       checkoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
@@ -65,51 +77,54 @@ class CartViewController: UIViewController {
 
 // MARK: - UITableViewDataSource, UITableViewDelegate
 extension CartViewController: UITableViewDataSource, UITableViewDelegate {
-  
+
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return cartProducts.count
+    return viewModel.cartItems.count
   }
-  
+
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(withIdentifier: "CartCell", for: indexPath) as? CartTableViewCell else {
       return UITableViewCell()
     }
-    
-    let item = cartProducts[indexPath.row]
+
+    let item = viewModel.cartItems[indexPath.row]
     cell.configure(with: item)
     cell.delegate = self
     cell.deleteAction = { [weak self] in
-      self?.removeItem(at: indexPath.row)
+      self?.viewModel.removeProductFromCart(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
     }
     return cell
   }
 
   func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-      let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] _, _, completionHandler in
-          self?.removeItem(at: indexPath.row)
-          completionHandler(true)
-      }
-
-      deleteAction.backgroundColor = .systemRed
-      return UISwipeActionsConfiguration(actions: [deleteAction])
+    let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] _, _, completionHandler in
+      self?.viewModel.removeProductFromCart(at: indexPath.row)
+      completionHandler(true)
+    }
+    deleteAction.backgroundColor = .systemRed
+    return UISwipeActionsConfiguration(actions: [deleteAction])
   }
-
-  func removeItem(at index: Int) {
-    guard index < cartProducts.count else { return }
-
-      cartProducts.remove(at: index)
-
-      tableView.performBatchUpdates({
-          tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .fade)
-      }, completion: nil)
-  }
+//
+//  func removeItem(at index: Int) {
+//    guard index < cartProducts.count else { return }
+//
+//    cartProducts.remove(at: index)
+//
+//    tableView.performBatchUpdates({
+//      tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .fade)
+//    }, completion: nil)
+//  }
 }
 
 // MARK: - CartTableViewCellDelegate
 extension CartViewController: CartTableViewCellDelegate {
-    func didTapDeleteButton(in cell: CartTableViewCell) {
-        guard let indexPath = tableView.indexPath(for: cell) else { return }
-// trigger swipe to delete
-      removeItem(at: indexPath.row)
-    }
+  func didTapDeleteButton(in cell: CartTableViewCell) {
+    guard let indexPath = tableView.indexPath(for: cell), indexPath.row < viewModel.cartItems.count  else { return }
+    // trigger swipe to delete
+    viewModel.removeProductFromCart(at: indexPath.row)
+        tableView.performBatchUpdates({
+          tableView.deleteRows(at: [IndexPath(row: indexPath.row, section: 0)], with: .fade)
+        }, completion: nil)
+  }
 }
---
+++ AttentiveExample/AttentiveExample/ProductListViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  ProductListViewModel.swift
+//  AttentiveExample
+//
+//  Created by Adela Gao on 3/5/25.
+//
+
+import Foundation
+import ATTNSDKFramework
+
+class ProductListViewModel {
+
+  // MARK: - Properties
+
+    private(set) var products: [ATTNItem] = []
+    private(set) var cartItems: [ATTNItem] = []
+
+    // MARK: - Initialization
+
+    init() {
+        setupProducts()
+    }
+
+    private func setupProducts() {
+        products = [
+            ATTNItem(productId: "1", productVariantId: "1A", price: ATTNPrice(price: NSDecimalNumber(string: "19.99"), currency: "USD")),
+            ATTNItem(productId: "2", productVariantId: "2A", price: ATTNPrice(price: NSDecimalNumber(string: "24.99"), currency: "USD")),
+            ATTNItem(productId: "3", productVariantId: "3A", price: ATTNPrice(price: NSDecimalNumber(string: "14.99"), currency: "USD")),
+            ATTNItem(productId: "4", productVariantId: "4A", price: ATTNPrice(price: NSDecimalNumber(string: "29.99"), currency: "USD")),
+            ATTNItem(productId: "5", productVariantId: "5A", price: ATTNPrice(price: NSDecimalNumber(string: "9.99"), currency: "USD")),
+            ATTNItem(productId: "6", productVariantId: "6A", price: ATTNPrice(price: NSDecimalNumber(string: "39.99"), currency: "USD"))
+        ]
+        assignMockDataToProducts()
+    }
+
+    private func assignMockDataToProducts() {
+        let sampleImages = [
+            "https://via.placeholder.com/150",
+            "https://via.placeholder.com/150/0000FF",
+            "https://via.placeholder.com/150/008000",
+            "https://via.placeholder.com/150/FF0000",
+            "https://via.placeholder.com/150/800080",
+            "https://via.placeholder.com/150/FFA500"
+        ]
+
+        for (index, product) in products.enumerated() {
+            product.name = "Product \(index + 1)"
+            product.productImage = sampleImages[index % sampleImages.count]
+        }
+    }
+
+    // MARK: - Cart Operations
+
+    func addProductToCart(_ product: ATTNItem) {
+        cartItems.append(product)
+    }
+
+    func removeProductFromCart(at index: Int) {
+        guard index < cartItems.count else { return }
+        cartItems.remove(at: index)
+    }
+}
