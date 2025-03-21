b0b8086b796fd9f0805babd3450c01a9910dee18
Adela Gao <adelag@attentivemobile.com>
2025-03-21 10:41:43 -0700

    added empty debug console vc, will resume after modifying sdk log callback

---
+++ AttentiveExample/AttentiveExample/DebugConsoleViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DebugConsoleViewController.swift
+//  AttentiveExample
+//
+//  Created by Adela Gao on 3/17/25.
+//
+
+import UIKit
+
+class DebugConsoleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      view.backgroundColor = .white
+      //make view semi transparent
+      view.alpha = 0.8
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
