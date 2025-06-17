2dd72e2ee4193a1e9ca1d0e1eb682711b767d443
Adela Gao <adelag@attentivemobile.com>
2025-06-17 09:21:42 -0700

    added one ui test

---
+++ Bonni/BonniUITests/BonniUITestsLaunchTests.swift
@@ -0,0 +1,43 @@
+//
+//  BonniUITestsLaunchTests.swift
+//  BonniUITests
+//
+//  Created by Adela Gao on 6/17/25.
+//
+
+import XCTest
+
+final class BonniUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
+
+import XCTest
+
+final class AppLaunchTests: XCTestCase {
+    func testLaunchPerformance() throws {
+        measure(metrics: [XCTApplicationLaunchMetric()]) {
+            XCUIApplication().launch()
+        }
+    }
+}
