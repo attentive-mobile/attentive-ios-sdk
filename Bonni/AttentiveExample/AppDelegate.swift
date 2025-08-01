4f7eedca3c067b59291b1973331133c3dc4eed56
Adela Gao <adelag@attentivemobile.com>
2025-08-01 12:47:09 -0700

    Revert "reversed unnecessary changes on AppDelegate"
    This reverts commit 597b22419cc0dc14e093dc5ad35dda606f932536.

--- Bonni/AttentiveExample/AppDelegate.swift
+++ Bonni/AttentiveExample/AppDelegate.swift
@@ -18,16 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     initializeAttentiveSdk()
-
-    UNUserNotificationCenter.current().getNotificationSettings { settings in
-      let authStatus = settings.authorizationStatus
-      if let remoteUserInfo = launchOptions?[.remoteNotification] as? [AnyHashable: Any] {
-        self.attentiveSdk?.handlePushOpen(userInfo: remoteUserInfo, authorizationStatus: authStatus)
-    }
-
-    }
-
     UNUserNotificationCenter.current().delegate = self
+    // Show push permission prompt
     attentiveSdk?.registerForPushNotifications()
     return true
   }
@@ -36,20 +28,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Intialize the Attentive SDK. Replace with your Attentive domain to test
     // with your Attentive account.
     // This only has to be done once per application lifecycle
-    ATTNSDK.initialize(domain: "YOUR_ATTENTIVE_DOMAIN", mode: .production) { result in
-      switch result {
-      case .success(let sdk):
-        self.attentiveSdk = sdk
-        ATTNEventTracker.setup(with: sdk)
+    let sdk = ATTNSDK(domain: "YOUR_ATTENTIVE_DOMAIN", mode: .production)
+    attentiveSdk = sdk
 
-        // Register the current user with the Attentive SDK by calling the `identify` method. Each identifier is optional, but the more identifiers you provide the better the Attentive SDK will function.
-        // Every time any identifiers are added/changed, call the SDK's "identify" method
-        sdk.identify(AppDelegate.createUserIdentifiers())
-      case .failure(let error):
-        // Handle init failure
-        print("Attentive SDK failed to initialize: \(error)")
-      }
-    }
+    // Initialize the ATTNEventTracker. This must be done before the ATTNEventTracker can be used to send any events. It only has to be done once per applicaiton lifecycle.
+    ATTNEventTracker.setup(with: sdk)
+
+    // Register the current user with the Attentive SDK by calling the `identify` method. Each identifier is optional, but the more identifiers you provide the better the Attentive SDK will function.
+    // Every time any identifiers are added/changed, call the SDK's "identify" method
+    sdk.identify(AppDelegate.createUserIdentifiers())
   }
 
   func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
@@ -57,17 +44,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       guard let self = self else { return }
       let authStatus = settings.authorizationStatus
       attentiveSdk?.registerDeviceToken(deviceToken, authorizationStatus: authStatus, callback: { data, url, response, error in
-
         DispatchQueue.main.async {
           self.attentiveSdk?.handleRegularOpen(authorizationStatus: authStatus)
         }
       })
-
-      //self.attentiveSdk?.registerDeviceToken(deviceToken,
-                                             //authorizationStatus: authStatus)
     }
 
-    // Store device token as string for display on settings screen. NOT Needed for client apps.
+    // Please disregard below. These are for internal debugging purposes only.
+    // Store device token as string for display on settings screen.
     let tokenString = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
 
     UserDefaults.standard.set(tokenString, forKey: "deviceTokenForDisplay")
@@ -83,7 +67,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   public static func createUserIdentifiers() -> [String: Any] {
     [
-      ATTNIdentifierType.phone: "+15671230987",
+      ATTNIdentifierType.phone: "",
       ATTNIdentifierType.email: "someemail@email.com",
       ATTNIdentifierType.clientUserId: "APP_USER_ID",
       ATTNIdentifierType.shopifyId: "207119551",
@@ -95,33 +79,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
   func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-    let userInfo = notification.request.content.userInfo
-    attentiveSdk?.handleForegroundNotification(userInfo, completionHandler: completionHandler)
+    let presentationOptions: UNNotificationPresentationOptions = [.alert, .sound, .badge]
+    completionHandler(presentationOptions)
   }
   func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-    let userInfo = response.notification.request.content.userInfo
 
     UNUserNotificationCenter.current().getNotificationSettings { settings in
       let authStatus = settings.authorizationStatus
       DispatchQueue.main.async {
         switch UIApplication.shared.applicationState {
         case .active:
-          // App was open when push was tapped
-          self.attentiveSdk?.handleForegroundPush(userInfo: userInfo, authorizationStatus: authStatus)
+          self.attentiveSdk?.handleForegroundPush(response: response, authorizationStatus: authStatus)
 
         case .background, .inactive:
-          // App was backgrounded or cold-launched
-          self.attentiveSdk?.handlePushOpen(userInfo: userInfo, authorizationStatus: authStatus)
+          self.attentiveSdk?.handlePushOpen(response: response, authorizationStatus: authStatus)
 
         @unknown default:
-          self.attentiveSdk?.handlePushOpen(userInfo: userInfo, authorizationStatus: authStatus)
+          self.attentiveSdk?.handlePushOpen(response: response, authorizationStatus: authStatus)
         }
       }
-
-
     }
-
-    
     completionHandler()
   }
 }
