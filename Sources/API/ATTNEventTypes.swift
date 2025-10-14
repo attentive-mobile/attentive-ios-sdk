bb85ecd8a785a9ca8a69a9d47ab82459e7bd7d3e
Adela Gao <adelag@attentivemobile.com>
2025-10-14 10:10:16 -0700

    v2 wip

--- Sources/API/ATTNEventTypes.swift
+++ Sources/API/ATTNEventTypes.swift
@@ -16,3 +16,71 @@ enum ATTNEventTypes {
   static var info: String { "i" }
   static var customEvent: String { "ce" }
 }
+
+// For new /mobile endpoint
+
+enum ATTNEventType: String, Codable {
+    case purchase = "p"
+    case addToCart = "c"
+    case productView = "d"
+}
+
+struct ATTNIdentifiers: Codable {
+    let encryptedEmail: String?
+    let encryptedPhone: String?
+    let otherIdentifiers: [[String: String]]?
+}
+
+struct ATTNProduct: Codable {
+    let productId: String
+    let variantId: String?
+    let name: String
+    let variantName: String?
+    let imageUrl: String?
+    let categories: [String]?
+    let price: String
+    let quantity: Int
+    let productUrl: String?
+}
+
+struct ATTNCart: Codable {
+    let cartId: String?
+    let cartTotal: String?
+    let cartCoupon: String?
+    let cartDiscount: String?
+}
+
+struct ATTNAddToCartMetadata: Codable {
+    let eventType: String = "AddToCart"
+    let product: ATTNProduct
+    let currency: String
+}
+
+struct ATTNProductViewMetadata: Codable {
+    let eventType: String = "ProductView"
+    let product: ATTNProduct
+    let currency: String
+}
+
+struct ATTNPurchaseMetadata { //: Codable {
+    let eventType: String = "Purchase"
+    let orderId: String
+    let currency: String
+    let orderTotal: String
+    let cart: ATTNCart?
+    let products: [ATTNProduct]
+}
+
+struct ATTNBaseEvent<M: Codable>: Codable {
+    let visitorId: String
+    let version: String
+    let attentiveDomain: String
+    let locationHref: String?
+    let referrer: String
+    let eventType: ATTNEventType
+    let timestamp: String
+    let identifiers: ATTNIdentifiers
+    let eventMetadata: M
+    let sourceType: String = "mobile"
+    let appSdk: String = "iOS"
+}
--- Sources/URLProviders/ATTNEventURLProvider.swift
+++ Sources/URLProviders/ATTNEventURLProvider.swift
@@ -10,6 +10,7 @@ import Foundation
 protocol ATTNEventURLProviding {
   func buildUrl(for userIdentity: ATTNUserIdentity, domain: String) -> URL?
   func buildUrl(for eventRequest: ATTNEventRequest, userIdentity: ATTNUserIdentity, domain: String) -> URL?
+  func buildNewEventEndpointUrl(for eventRequest: ATTNEventRequest, userIdentity: ATTNUserIdentity, domain: String) -> URL?
   func buildPushTokenUrl(for userIdentity: ATTNUserIdentity, domain: String) -> URL?
 }
 
@@ -18,6 +19,7 @@ struct ATTNEventURLProvider: ATTNEventURLProviding {
     static var scheme: String { "https" }
     static var host: String { "events.attentivemobile.com" }
     static var path: String { "/e" }
+    static var newEventPath: String { "/mobile" }
 
     static var pushHost: String { "mobile.attentivemobile.com" }
     static var pushPath: String { "/token" }
@@ -52,6 +54,23 @@ struct ATTNEventURLProvider: ATTNEventURLProviding {
     return urlComponents.url
   }
 
+  func buildNewEventEndpointUrl(for eventRequest: ATTNEventRequest, userIdentity: ATTNUserIdentity, domain: String) -> URL? {
+    var urlComponents = getNewEventEndpointUrlComponent()
+    // TODO finish building url for new event endpoint
+    var queryParams = userIdentity.constructBaseQueryParams(domain: domain)
+    var combinedMetadata = userIdentity.buildBaseMetadata() as [String: Any]
+    combinedMetadata.merge(eventRequest.metadata) { (current, _) in current }
+    queryParams["m"] = try? ATTNJsonUtils.convertObjectToJson(combinedMetadata) ?? "{}"
+    queryParams["t"] = eventRequest.eventNameAbbreviation
+
+    if let deeplink = eventRequest.deeplink {
+      queryParams["pd"] = deeplink
+    }
+
+    urlComponents.queryItems = queryParams.map { .init(name: $0.key, value: $0.value) }
+    return urlComponents.url
+  }
+
   func buildPushTokenUrl(for userIdentity: ATTNUserIdentity, domain: String) -> URL? {
     var components = getUrlComponent(
       host: Constants.pushHost,
@@ -68,6 +87,14 @@ extension ATTNEventURLProvider {
     urlComponent.scheme = Constants.scheme
     urlComponent.host = Constants.host
     urlComponent.path = Constants.path
+    return urlComponent
+  }
+
+  private func getNewEventEndpointUrlComponent() -> URLComponents {
+    var urlComponent = URLComponents()
+    urlComponent.scheme = Constants.scheme
+    urlComponent.host = Constants.host
+    urlComponent.path = Constants.newEventPath
     return urlComponent
   }
 
