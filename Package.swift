101b11cfee8842424a2263ff99d4b40c82e51927
usharif-attentive <usharif@attentivemobile.com>
2026-01-23 18:09:10 -0800

    bumps package value to iOS 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ATTNSDKFramework",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "ATTNSDKFramework", targets: ["ATTNSDKFramework", "ATTNSDKFrameworkObjc"])
     ],
