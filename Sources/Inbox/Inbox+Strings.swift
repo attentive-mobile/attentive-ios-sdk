4dd587c06fe53c8e1330c832fc5d21ee57edeab1
usharif-attentive <usharif@attentivemobile.com>
2026-01-27 17:02:01 -0800

    adds strings

---
+++ Sources/Inbox/Inbox+Strings.swift
@@ -0,0 +1,32 @@
+//
+//  Inbox+Strings.swift
+//  attentive-ios-sdk
+//
+//  Created by Umair Sharif on 1/27/26.
+//
+
+extension String {
+    static var inbox: String {
+        String(localized: "Inbox")
+    }
+
+    static var noMessages: String {
+        String(localized: "No messages")
+    }
+
+    static var delete: String {
+        String(localized: "Delete")
+    }
+
+    static var read: String {
+        String(localized: "Read")
+    }
+
+    static var unread: String {
+        String(localized: "Unread")
+    }
+    
+    static var somethingWentWrong: String {
+        String(localized: "Something went wrong. Please try again.")
+    }
+}
