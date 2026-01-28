d639dad4a7e672a19d13e96f300883514eb14804
adelag-attentive <adelag@attentivemobile.com>
2026-01-28 14:03:58 -0800

    version number update (#172)

--- Sources/ATTNConstants.swift
+++ Sources/ATTNConstants.swift
@@ -10,6 +10,6 @@ import Foundation
 struct ATTNConstants {
     private init() { }
 
-    static let sdkVersion = "2.0.8"
+    static let sdkVersion = "2.0.9"
     static let skipFatigueEnvKey = "SKIP_FATIGUE_ON_CREATIVE"
 }
